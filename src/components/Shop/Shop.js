fc2d8ed4133065bd96acf3fe54734b7a001910ab
unknown <farjana015215@gmail.com>
2021-05-18 14:50:34 +0600

    fourth commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,13 +6,20 @@ import './Shop.css';
 const Shop = () => {
     const first10= fakeData.slice(0,10);
     const [products,setProducts] = useState(first10);
+
+    const handleAddProduct =(product) =>{
+        console.log('Product added',product);
+    }
     
     return (
         <div className="shop-container">
             <div className="product-container">
               
                 {
-                    products.map(pd =><Products product={pd}></Products>)
+                    products.map(pd =><Products
+                        handleAddProduct ={handleAddProduct} 
+                    product={pd}>
+                    </Products>)
                 }
              </div>
              <div className="cart-container">
